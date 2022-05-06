9a7658ce4d07291b77a0a6c21655d649b4d59e29
opatricpires <patric.pires@outlook.com>
2022-05-06 09:37:04 -0300

    feat(widget): create initial component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef } from "react";
+import { TouchableOpacity, Text } from "react-native";
 import { styles } from "./styles";
 
-export function Widget() {
-  return <View style={styles.container}></View>;
+import { ChatTeardropDots } from "phosphor-react-native";
+import { theme } from "../../theme";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { Options } from "../Options";
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
+
+  return (
+    <>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color}
+          weight="light"
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
+    </>
+  );
 }
+
+export default Widget;
--- src/components/Widget/styles.ts
+++ src/components/Widget/styles.ts
@@ -1,10 +1,25 @@
 import { StyleSheet } from "react-native";
 import { theme } from "../../theme";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 export const styles = StyleSheet.create({
-  container: {
-    backgroundColor: theme.colors.background,
-    width: 20,
-    height: 20,
+  button: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: theme.colors.brand,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 16,
+    bottom: getBottomSpace() + 16,
+  },
+  modal: {
+    backgroundColor: theme.colors.surface_primary,
+    paddingBottom: getBottomSpace() + 16,
+  },
+  indicator: {
+    backgroundColor: theme.colors.text_primary,
+    width: 56,
   },
 });
